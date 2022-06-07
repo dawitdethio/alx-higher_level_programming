c58959fb683052d48d0683f03e58251a70a3c56a
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-06-07 19:30:13 +0300

    no cpy

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return
-    strlen = len(my_string)
-    new_string = ""
-    for i in range(strlen):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-                new_string += my_string[i]
-    return (new_string)
+    new_str = ""
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new_str += i
+    return (new_str)
+# return my_string.translate({ord(c): None for c in "cC"})
