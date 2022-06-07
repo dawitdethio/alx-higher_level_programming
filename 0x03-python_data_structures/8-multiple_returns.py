66cdf1c5545bcd72c75d1bd6f2b4fd849b386744
dawitdethio <dawitdereje07@gmail.com>
2022-06-07 09:20:52 -0700

    threee

--- 0x03-python_data_structures/8-multiple_returns.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    lens = len(sentence)
-    if (lens == 0):
-        return (0, None)
-    return (lens, sentence[0])
--- 0x03-python_data_structures/README.md
+++
@@ -1,1 +0,0 @@
-alx_higher_level_programming
