d9217bd1f80cff023d36dcb8c5aa19b9ab20becd
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-06-30 10:13:45 +0300

    one sqr

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" This module defines a class 'Square' """
+
+
 class Square:
-    def __init__(self, size=0):
+    """ Definition of a square by its size. """
+
+    def __init__(self, size):
+        """ Initialize size attribute. """
         self.__size = size
