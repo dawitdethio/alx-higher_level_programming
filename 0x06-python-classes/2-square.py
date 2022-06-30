4438c96acf76f4ac4d4ebbfc96586cf1431e3d5e
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-06-30 10:16:18 +0300

    tow sqr

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,56 @@
 #!/usr/bin/python3
+""" This module defines a class 'Square'. """
+
+
 class Square:
+    """Represents a square.
+    Private instance attribute: size:
+        - property def size(self)
+        - property setter def size(self, value)
+    Instantiation with optional size.
+    Public instance method: def area(self).
+    """
+
     def __init__(self, size=0):
-        if type(size) != int:
-            print("size must be an integer", end="")
-            raise TypeError
-        if size >= 0:
-            self.__size = size
-        else:
-            print("size must be >= 0", end="")
-            raise 
+        """ Initialize size. """
+        self.__size = size
+
+    def __eq__(self, other):
+        """ Equal. """
+        if hasattr(other, 'size'):
+            return self.__size == other.__size
+        return self.__size == other
+
+    def __ne__(self, other):
+        """ Not equal. """
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        """ Less than. """
+        if hasattr(other, 'size'):
+            return self.__size < other.__size
+        return self.__size < other
+
+    def __le__(self, other):
+        """ Less than or equal. """
+        if hasattr(other, 'size'):
+            return self.__size <= other.__size
+        return self.__size <= other
+
+    @property
+    def size(self):
+        """ Get size. """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Set the size to equal value. """
+        if not isinstance(value, int) or not isinstance(value, float):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Return area of the square."""
+        return self.__size ** 2
