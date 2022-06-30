4b6328e8efd5dc42a1f9660af6428000b3737d73
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-06-30 10:19:14 +0300

    tree sqr

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" This module defines a class 'Square' """
+
+
 class Square:
+    """ Definition of a square by its size. """
+
     def __init__(self, size=0):
+        """ Initialize size attribute with validation. """
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size >= 0:
-            self.__size = size
-        else:
-            print("size must be >= 0", end="")
-            raise ValueError
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        size = self.__size
-        return size * 
+        """ Return the area of the square. """
+        return self.__size ** 2
