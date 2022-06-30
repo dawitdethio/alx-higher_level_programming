5773c4466fe431abea72d0b185845fcf46dc05dd
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-06-30 10:21:33 +0300

    fiv sqr

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
+""" This module defines a class 'Square' """
+
+
 class Square:
+    """ Definition of a square by its size. """
+
     def __init__(self, size=0):
-        self.size = size
+        """ Initialize size attribute. """
+        self.__size = size
 
     @property
     def size(self):
+        """ Get size. """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Set size to equal value. """
         if type(value) != int:
             raise TypeError("size must be an integer")
-        if value >= 0:
-            self.__size = value
-        else:
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        size = self.size
-        return size * size
+        """ Return the area of the square. """
+        return self.__size ** 2
 
     def my_print(self):
-        size = self.size
-        if (size == 0):
-            print("")
-        else:
-            for i in range(size):
-                print("#" * size)
+        " Print the square with the character #. "
+        if self.__size == 0:
+            print()
+        [print("#" * self.__size) for i in range(self.__size)]
